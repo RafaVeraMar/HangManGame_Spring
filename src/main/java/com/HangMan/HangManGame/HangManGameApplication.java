d7f473997b53210e9f7ce0cc1af502539cf265bb
RafaVera <rafael.vera@babelgroup.com>
2023-03-07 13:22:39 +0100

    Initial commit

---
+++ src/main/java/com/HangMan/HangManGame/HangManGameApplication.java
@@ -0,0 +1,13 @@
+package com.HangMan.HangManGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangManGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangManGameApplication.class, args);
+	}
+
+}
