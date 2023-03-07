651c5066d075384a2af87177bd98479bc896bef8
RafaVera <rafael.vera@babelgroup.com>
2023-03-07 14:05:15 +0100

    siguienddo

--- src/main/java/com/HangMan/HangManGame/Repository/Words.java
+++ src/main/java/com/HangMan/HangManGame/Repository/Words.java
@@ -31,10 +31,33 @@ public class Words {
     public void nuevoJuego(){
         nbErrors = 0;
         letras.clear();
+        wordToFind = nextWordToFind();
+
+        // inicializacion de la palabra elegida
+        wordFound = new char[wordToFind.length()];
+        for (int i = 0; i <wordFound.length; i++){
+            wordFound[i] = '_';
+        }
 
 
     }
 
+    // Metodo devolviendo trus si la palabra es acertada por el usuario
+    public boolean wordFound(){
+        return wordToFind.contentEquals(new String(wordFound));
+
+    }
+
+    // metodo que actualiza la palabra encontrada despues de ue usuario introduzca un caracter
+    public void  enter(char c){
+        // we update only if c has not already been entered
+        if (!letras.contains(c)){
+            // we check if word to find contains c
+            if (wordToFind.contains(c + "")) {
+                //MINUTO 5:54
+            }
+        }
+    }
 
 
 
